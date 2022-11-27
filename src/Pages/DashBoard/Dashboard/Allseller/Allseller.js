a4e5622ee7cf5a56a15ebf48082a1bd2ea3e1f32
Zahid Hasan Akram <Mohammadibrahim2>
2022-11-28 00:44:56 +0600

    after complete the all user section of dashboard

--- src/Pages/DashBoard/Dashboard/Allseller/Allseller.js
+++ src/Pages/DashBoard/Dashboard/Allseller/Allseller.js
@@ -1,65 +1,62 @@
 
-import React  from "react";
-import { useLoaderData } from "react-router-dom";
+// import React  from "react";
+// import { useLoaderData } from "react-router-dom";
 
-const Allseller=()=>{
+// const Allseller=()=>{
  
-const Sellers=useLoaderData()
-console.log(Sellers,"hello vai")
+// const Sellers=useLoaderData()
+// console.log(Sellers,"hello vai")
 
 
 
 
       
   
-    return(
-        <div>
-            <h1>Ahwllow sellers</h1>
-            <div> <h2 className="text-3xl bg-white text-black">All sellers</h2></div>
-            <div className="overflow-x-auto text-xl ">
-  <table className="table text-black w-full  ">
-    <thead className="bg-red-10 ">
-      <tr className="">
-        <th className=" text-xl bg-orange-100"></th> 
-        <th className=" text-xl bg-orange-100">Name</th> 
-        <th className=" text-xl bg-orange-100">Email</th> 
-        <th className=" text-xl bg-orange-100">location</th> 
-        <th className=" text-xl bg-orange-100">Product Name</th> 
-        <th className=" text-xl bg-orange-100">price</th> 
-        <th className=" text-xl bg-orange-100">phone</th> 
+//     return(
+//         <div>
+//             <h1>Ahwllow sellers</h1>
+//             <div> <h2 className="text-3xl bg-white text-black">All sellers</h2></div>
+//             <div className="overflow-x-auto text-xl ">
+//   <table className="table text-black w-full  ">
+//     <thead className="bg-red-10 ">
+//       <tr className="">
+//         <th className=" text-xl bg-orange-100"></th> 
+//         <th className=" text-xl bg-orange-100">Name</th> 
+//         <th className=" text-xl bg-orange-100">Email</th> 
+         
        
-      </tr>
-    </thead> 
-    <tbody className="mt-5 text-left text-white">
+//       </tr>
+//     </thead> 
+//     <tbody className="mt-5 text-left text-white">
     
 
 
-{
-    Sellers.map((seller,i)=><tr className="py-5">
-    <th></th> 
+// {
+//     Sellers.map((seller,i)=><tr className="py-5">
+//     <th></th> 
    
-    <td>
-  <div className="flex items-center space-x-3">
-    <div className="avatar">
-      <div className="mask mask-squircle w-12 h-12">
-        <img src={seller.photoURL} alt="Avatar Tailwind CSS Component" />
-      </div>
-    </div>
+//     <td>
+//   <div className="flex items-center space-x-3">
+//     <div className="avatar">
+//       <div className="mask mask-squircle w-12 h-12">
+//         <img src={seller.photoURL} alt="Avatar Tailwind CSS Component" />
+//       </div>
+//     </div>
     
-  </div>
-</td>
-<th>{seller.name}</th> 
-    <th>{seller.email}</th> 
-    <th>{seller.role}</th> 
+//   </div>
+// </td>
+// <th>{seller.name}</th> 
+//     <th>{seller.email}</th> 
+//     <th>{seller.role}</th> 
    
-  </tr>)
-}
+//   </tr>)
+// }
 
-</tbody> 
+// </tbody> 
     
-    </table>
-  </div>
-        </div>
-    )
-}
-export default Allseller
+//     </table>
+//   </div>
+//         </div>
+//     )
+// }
+// export default Allseller
