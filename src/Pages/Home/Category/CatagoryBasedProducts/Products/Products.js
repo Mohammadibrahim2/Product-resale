4cd2bf6315922ebea8d8f26c99a066277e53bf1c
Zahid Hasan Akram <Mohammadibrahim2>
2022-11-28 00:19:05 +0600

    after setup the catagory based product section

--- src/Pages/Home/Category/CatagoryBasedProducts/Products/Products.js
+++ src/Pages/Home/Category/CatagoryBasedProducts/Products/Products.js
@@ -1,39 +1,39 @@
-// import React, { useState } from "react";
-// import { useLoaderData } from "react-router-dom";
-// import SingleProduct from "../SinglePoruct";
-// import BookingModal from "./BookingModal/BookingModal";
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import SingleProduct from "../SinglePoruct";
+import BookingModal from "./BookingModal/BookingModal";
 
-// const ProductsOFCategory=()=>{
-//     const [modalData,setModalData]=useState(null)
-//     const productsData=useLoaderData()
-
-
-// return(
-//     <div>
-//          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 px-5 my-10">
-//         {
-//           productsData.map((product,i)=><SingleProduct
-//           key={i}
-//           product={product}
-//           setModalData={setModalData}
-//           ></SingleProduct>)  
-//         }
+const ProductsOFCategory=()=>{
+    const [modalData,setModalData]=useState(null)
+    const productsData=useLoaderData()
 
 
-//     </div>
-//     {modalData && 
-//          <BookingModal 
-//          modalData={modalData}
+return(
+    <div>
+         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 px-5 my-10">
+        {
+          productsData.map((product,i)=><SingleProduct
+          key={i}
+          product={product}
+          setModalData={setModalData}
+          ></SingleProduct>)  
+        }
+
+
+    </div>
+    {modalData && 
+         <BookingModal 
+         modalData={modalData}
        
-//          setModalData={setModalData}
+         setModalData={setModalData}
          
-//          ></BookingModal>
-//         }
+         ></BookingModal>
+        }
        
 
-//     </div>
+    </div>
    
-// )
+)
 
-// }
-// export default  ProductsOFCategory
+}
+export default  ProductsOFCategory
