6de657fac4835a5e5b03e6edc4dbd8a5f10d52d2
Zahid Hasan Akram <Mohammadibrahim2>
2022-11-28 00:26:54 +0600

    after comoplete the registration form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,104 +1,104 @@
-// import { GoogleAuthProvider } from "firebase/auth";
-// import React, { useContext, useState } from "react";
-// import { Link, useLocation, useNavigate } from "react-router-dom";
-// import useToken from "../../../Hooks/useToken";
-// import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
-// import "./Login.css";
-// const Googleprovider = new GoogleAuthProvider();
+import { GoogleAuthProvider } from "firebase/auth";
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useToken from "../../../Hooks/useToken";
+import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
+import "./Login.css";
+const Googleprovider = new GoogleAuthProvider();
 
-// const Login = () => {
-//     const navigate=useNavigate()
-//     const location=useLocation()
-//     const [loginUserEmail, setloginUserEmail] = useState('')
-//     const [token] = useToken(loginUserEmail);
+const Login = () => {
+    const navigate=useNavigate()
+    const location=useLocation()
+    const [loginUserEmail, setloginUserEmail] = useState('')
+    const [token] = useToken(loginUserEmail);
     
    
     
-//     const from=location.state?.from?.pathname || "/"
-//     if(token){
-//         navigate(from,{replace:true})
-//     }
+    const from=location.state?.from?.pathname || "/"
+    if(token){
+        navigate(from,{replace:true})
+    }
 
-//     const { login, GooglessigninProvider } = useContext(AuthContext);
+    const { login, GooglessigninProvider } = useContext(AuthContext);
 
-//     const handleGooglelogin = () => {
+    const handleGooglelogin = () => {
 
-//         GooglessigninProvider(Googleprovider)
-//             .then(result => {
-//                 const user = result.user
-//                 console.log(user)
-//             })
-//     }
+        GooglessigninProvider(Googleprovider)
+            .then(result => {
+                const user = result.user
+                console.log(user)
+            })
+    }
 
-//     const handlesubmit = (event) => {
-//         event.preventDefault()
-//         const form = event.target;
-//         const email = form.email.value;
-//         const password = form.password.value;
-//         const userInfo = {
+    const handlesubmit = (event) => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const userInfo = {
 
-//             Email: email,
-//             Password: password
-//         }
+            Email: email,
+            Password: password
+        }
 
-//         login(email, password)
-//             .then(result => {
-//                 const user = result.user
-//                 console.log(user)
-//                 setloginUserEmail(email)
+        login(email, password)
+            .then(result => {
+                const user = result.user
+                console.log(user)
+                setloginUserEmail(email)
                
               
 
-//             })
-//         form.reset()
+            })
+        form.reset()
 
 
-//     }
+    }
 
-//     return (
-//         <div>
-//             <div className="hero min-h-screen loginbg">
-//                 <div className="hero-content flex lg:flex-row-reverse">
-//                     <div className="text-center lg:text-left text-white ">
-//                         {/* <h1 className="text-5xl font-bold">Login now!</h1>
-//                         <p className="py-6">Wellcome back!.</p> */}
-//                     </div>
-//                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-//                         <form onSubmit={handlesubmit} className="p-10 formbg w-full">
-//                             <div className="text-center w-full">
-//                                 <img className="w-40 rounded " src="https://www.nicepng.com/png/detail/302-3026464_png-file-svg-login-member-icon-png.png" />
-//                             </div>
+    return (
+        <div>
+            <div className="hero min-h-screen loginbg">
+                <div className="hero-content flex lg:flex-row-reverse">
+                    <div className="text-center lg:text-left text-white ">
+                        {/* <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">Wellcome back!.</p> */}
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handlesubmit} className="p-10 formbg w-full">
+                            <div className="text-center w-full">
+                                <img className="w-40 rounded " src="https://www.nicepng.com/png/detail/302-3026464_png-file-svg-login-member-icon-png.png" />
+                            </div>
 
-//                             <div className="form-control">
-//                                 <label className="label">
-//                                     <span className="label-text">Email</span>
-//                                 </label>
-//                                 <input type="email" name="email" placeholder="email" className="input input-bordered" />
-//                             </div>
-//                             <div className="form-control">
-//                                 <label className="label">
-//                                     <span className="label-text">Password</span>
-//                                 </label>
-//                                 <input type="password" name="password" placeholder="password" className="input input-bordered" />
-//                                 <label className="label">
-//                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-//                                 </label>
-//                             </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
                            
-//                             <div>Create a new account ? <Link className="text-green-500" to="/register">please Register</Link></div>
-//                             <div className="form-control mt-6">
-//                                 <button className="btn bg-green-500 text-black">Login</button>
-//                             </div>
-//                             <div className="form-control mt-6">
-//                                 <button onClick={handleGooglelogin} className="btn bg-green-500 text-black">Google Sign in</button>
-//                             </div>
-//                         </form>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
+                            <div>Create a new account ? <Link className="text-green-500" to="/register">please Register</Link></div>
+                            <div className="form-control mt-6">
+                                <button className="btn bg-green-500 text-black">Login</button>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button onClick={handleGooglelogin} className="btn bg-green-500 text-black">Google Sign in</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 
 
-// }
-// export default Login
+}
+export default Login
