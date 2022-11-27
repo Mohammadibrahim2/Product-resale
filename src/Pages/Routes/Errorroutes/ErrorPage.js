3890a101d7b2392111c5e8623ef810db2b67f8a1
Zahid Hasan Akram <Mohammadibrahim2>
2022-11-28 00:32:22 +0600

    the footer section is fullfilled

--- src/Pages/Routes/Errorroutes/ErrorPage.js
+++ src/Pages/Routes/Errorroutes/ErrorPage.js
@@ -1,15 +1,15 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-// const ErrorPage=()=>{
+const ErrorPage=()=>{
 
-// return(
-//     <div className="w-full">
-//         <img src="https://cdn.shopify.com/s/files/1/0533/2089/files/best-404-pages.jpg?v=1512659597" className="w-full"
-//         alt=""/>
-//         <h1 className="text-5xl text-orange-300 mb-2 font-extrabold">Page Not found</h1>
-//         <h2 className="text-3xl text-orange-300 font-bold "><Link to="/">Back to Home</Link></h2>
-//     </div>
-// )
-// }
-// export default ErrorPage
+return(
+    <div className="w-full">
+        <img src="https://cdn.shopify.com/s/files/1/0533/2089/files/best-404-pages.jpg?v=1512659597" className="w-full"
+        alt=""/>
+        <h1 className="text-5xl text-orange-300 mb-2 font-extrabold">Page Not found</h1>
+        <h2 className="text-3xl text-orange-300 font-bold "><Link to="/">Back to Home</Link></h2>
+    </div>
+)
+}
+export default ErrorPage
